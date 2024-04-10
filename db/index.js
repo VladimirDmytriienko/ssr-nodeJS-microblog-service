b09978dd397a2697b489515b589de84a48d25eee
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2024-04-10 22:29:02 +0300

    try to make docker with  postgress

--- db/index.js
+++ db/index.js
@@ -1,8 +1,6 @@
 const knexLib = require('knex');
-
-// initializing generic connection to our DB
 const knex = knexLib(
-    require('../knexfile')
+    require('../knexfile')[process.env.NODE_ENV]
 );
 
 module.exports = {
